bd602b7fdb054a60cf02c08125195b684b50fd86
MicaelaPerez02 <micaelaperez663@gmail.com>
2021-10-21 10:37:21 -0300

    Creando funcionalidad darkMode

---
+++ DarkModeButton/js/darkmode.js
@@ -0,0 +1,6 @@
+function darkMode() {
+    var element = document.body;
+    //seleccioname todo el body con classlist
+    //con el toggle si le digo q quiero el modo oscuro, me añade la clase dark, sino la saca.
+    element.classList.toggle("dark");
+ }
